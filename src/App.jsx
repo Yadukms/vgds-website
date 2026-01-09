ffcd1701c0fde2512bdbfe4df0541a6d13c942c4
Yadu <yadums2003@gmail.com>
2026-01-09 19:49:18 +0530

    Fix App structure: render Home page correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import Stats from "./components/Stats";
-import Solutions from "./components/Solutions";
-import AboutSection from "./components/AboutSection";
-import QuoteSection from "./components/QuoteSection";
-import ServicesSection from "./components/ServicesSection";
-import WhyVGDS from "./components/WhyVGDS";
-import FAQSection from "./components/FAQSection";
-import OurProcess from "./components/OurProcess";
-import Footer from "./components/Footer";
-function App() {
-  return (
-      <>
-      <Header />
-      <Hero/>
-      <Stats/>
-      <Solutions/>
-      <AboutSection/>
-      <QuoteSection/>
-      <ServicesSection/>
-      <WhyVGDS/>
-      <FAQSection/>
-      <OurProcess/>
-      <Footer/>
-    </>
+import Home from "./pages/Home";
 
-  )
+function App() {
+  return <Home />;
 }
 
-export default App
+export default App;
